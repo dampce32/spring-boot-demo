0657e00de02653f00fd9d9c4eb67edec067579f0
lys <linyisong032@gmail.com>
2017-11-21 14:13:16 +0800

    （+）集成thymeleaf

--- spring-boot-demo/src/main/java/org/lys/SpringBootDemoApplication.java
+++ spring-boot-demo/src/main/java/org/lys/SpringBootDemoApplication.java
@@ -1,14 +1,17 @@
 package org.lys;
 
-import org.lys.config.AuthorSettings;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.Banner.Mode;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
+//@RestController
 @SpringBootApplication
 public class SpringBootDemoApplication {
 
@@ -22,14 +25,32 @@ public class SpringBootDemoApplication {
 //		return "book name is:" + bookName + " and book author is:" + bookAuthor;
 //	}
 	
-	@Autowired
-	private AuthorSettings authorSettings; 
+//	@Autowired
+//	private AuthorSettings authorSettings; 
+//	
+//	@RequestMapping("/")
+//	public String index(){
+//		return "author name is "+ authorSettings.getName()+" and author age is "+authorSettings.getAge();
+//	}
+
 	
 	@RequestMapping("/")
-	public String index(){
-		return "author name is "+ authorSettings.getName()+" and author age is "+authorSettings.getAge();
+	public String index(Model model){
+		Person single = new Person("aa",11);
+		
+		List<Person> people = new ArrayList<Person>();
+		Person p1 = new Person("xx",11);
+		Person p2 = new Person("yy",22);
+		Person p3 = new Person("zz",33);
+		people.add(p1);
+		people.add(p2);
+		people.add(p3);
+		
+		model.addAttribute("singlePerson", single);
+		model.addAttribute("people", people);
+		
+		return "index";
 	}
-
 
 	public static void main(String[] args) {
 		SpringApplication app = new SpringApplication(SpringBootDemoApplication.class);
